524f2ea1e6ecccb9a10298adb1fc47ae289a5455
haohao.brian <brian.chunhao@gmail.com>
2025-08-31 11:22:59 +0800

    Update install_bril.sh

--- install_bril.sh
+++ install_bril.sh
@@ -1,13 +1,9 @@
 #1. install deno and show version
 curl -fsSL https://deno.land/install.sh | sh
 export PATH="~/.deno/bin:$PATH"
-git clone --recursive
- https://github.com/NTU-Advanced-Compiler/ntu-2025fall-advanced-compiler-hw1-haohao-brian.git
 deno --version
 
-#2. pull hw1 and install brili
-tar -xvzf NTU-Advanced-Compiler/ntu-2025fall-advanced-compiler-hw1-haohao-brian.tar.gz
-cd NTU-Advanced-Compiler/ntu-2025fall-advanced-compiler-hw1-haohao-brian
+#2. install brili
 deno install -g ./brili.ts
 
 #3. install flit
